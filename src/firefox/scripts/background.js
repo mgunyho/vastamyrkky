73cc075e0dac5384cfca0d8e23880259f7889966
Marton Gunyho <andras.gunyho@aalto.fi>
2019-11-05 00:04:28 +0200

    Prevent force download of PDF files by modifying headers

--- src/firefox/scripts/background.js
+++ src/firefox/scripts/background.js
@@ -2,3 +2,20 @@
 browser.browserAction.onClicked.addListener(function() {
 	browser.runtime.openOptionsPage();
 });
+
+//TODO: only do this if prevent force dl is enabled in settings
+browser.webRequest.onHeadersReceived.addListener(function(e) {
+	for(var header of e.responseHeaders) {
+		//console.log(header);
+		if(header.name.toLowerCase() === "content-disposition") {
+			var s = header.value.split(" ");
+			if(s[0] === "attachment;") {
+				header.value = "inline; " + s[1];
+				console.log(header);
+			}
+		}
+	}
+
+	return { responseHeaders: e.responseHeaders };
+
+}, {"urls": ["*://mycourses.aalto.fi/*"]}, ["blocking", "responseHeaders"]);
